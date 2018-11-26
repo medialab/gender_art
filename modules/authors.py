109a230f144dd1601d6186e6eb3b1ffe0d41b6f2
jdenes <julien50@sfr.fr>
2018-11-26 18:14:30 +0100

    Some modif on authors.py

--- modules/authors.py
+++ modules/authors.py
@@ -2,6 +2,7 @@ import pymongo
 import csvkit
 import re
 import cleaning
+import pandas as pd
 
 db = pymongo.MongoClient("localhost", 27017)["VideoMuseum"]# changed to VideoMuseum by Ruta Binkyte
 
@@ -34,19 +35,26 @@ project = {
 }
 
 # unique arworks
-authors = list(db.Author.aggregate([
-    {"$project":project}
-    ]))
+authors = list(db.Author.aggregate([{"$project":project}]))
+
 for author in authors:
     # cleaning acquisition mode
     if 'authors_birth_death' in author:
-        author.update(cleaning.artist_birthdeath_parsing(author['authors_birth_death']))
-        del author['authors_birth_death']
+       author.update(cleaning.artist_birthdeath_parsing(author['authors_birth_death']))
+       del author['authors_birth_death']
     author['artworks'] = '|'.join(author['artworks'])
     author['name.notice']= author["name"]["notice"]
     del author["name"]
 
-    # added encoding='utf-8' by ruta
+# df = pd.DataFrame(authors)
+# print(list(df.columns.values))
+# df.columns = [headers[c] for c in df.columns.values]
+# print(list(headers.values()))
+# df = df[list(headers.values())]
+# df.to_csv("authors.csv", encoding = "utf-8", sep = ";", index = False)
+
+
+# Added encoding='utf-8' by ruta
 with open("authors.csv", "w", encoding='utf-8') as f:
     authors_csv = csvkit.DictWriter(f,fieldnames = headers.values())
     authors_csv.writeheader()
