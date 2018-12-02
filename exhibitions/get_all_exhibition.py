b0104076a748ce259d7ed446926e2b11ae0cde79
jdenes <julien50@sfr.fr>
2018-12-03 00:56:40 +0100

    Finished script to retrieve all exhibitions

--- exhibitions/get_all_exhibition.py
+++ exhibitions/get_all_exhibition.py
@@ -6,7 +6,7 @@ from get_html_list import *
 field_dict = {'expositions':1}
 db = pymongo.MongoClient("localhost", 27017)["VideoMuseum"]
 cursor = db.myproject.Artwork.find({}, field_dict)
-all_artworks = db.Artwork.find({})
+all_artworks = db.Artwork.find({}, no_cursor_timeout = True)
 n = all_artworks.count()
 print("Number of artworks in database:", n)
 
@@ -31,5 +31,6 @@ for doc in all_artworks:
                 res = res.append(tmp, ignore_index = True)
     i += 1
 
+all_artworks.close()
 print("Done. Number of exhibitions found:", res.shape[0])
 res.to_csv("all_exhibitions.csv", sep = ";")
